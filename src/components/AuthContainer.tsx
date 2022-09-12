e1e48ada5b22195075e2a71d6837a8c70fa31717
hwanyb <mnn9502@naver.com>
2022-09-12 21:30:00 +0900

    Feat: - AuthContainer 회원가입기능 추가 - AuthContainer 로그인 기능 추가

--- src/components/AuthContainer.tsx
+++ src/components/AuthContainer.tsx
@@ -1,9 +1,108 @@
-import React from 'react'
+import React, { useState } from "react";
+import styled from "styled-components";
+import { authService, firebaseInstance } from "../firebase";
 
-type Props = {}
+const AuthWrapper = styled.div``;
+const AuthForm = styled.form``;
+const EmailInput = styled.input``;
+const PasswordInput = styled.input``;
+const ErrorText = styled.p``;
+const SignupButton = styled.button``;
+const SignupWithGoogle = styled.button``;
+export default function AuthContainer() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const [signupMode, setSignupMode] = useState(true);
 
-export default function AuthContainer({}: Props) {
+  const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const {
+      target: { name, value },
+    } = event;
+
+    if (name === "email") {
+      setEmail(value);
+    } else if (name === "password") {
+      setPassword(value);
+    }
+  };
+  const onSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    if (signupMode) {
+      await authService
+        .createUserWithEmailAndPassword(email, password)
+        .then(() => {
+          alert("회원가입 완료");
+        })
+        .catch((error) => setError(error.message));
+    } else {
+      await authService
+        .signInWithEmailAndPassword(email, password)
+        .then(() => {
+          alert("로그인 완료");
+        })
+        .catch((error) => setError(error.message));
+    }
+  };
+  const onGoogleSignupClick = async (event: React.MouseEvent) => {
+    event.preventDefault();
+    const provider = new firebaseInstance.auth.GoogleAuthProvider();
+    if (signupMode) {
+      await authService
+        .signInWithPopup(provider)
+        .then(() => {
+          alert("회원가입 완료");
+        })
+        .catch((error) => setError(error.message));
+    } else {
+      await authService
+        .signInWithPopup(provider)
+        .then(() => {
+          alert("로그인 완료");
+        })
+        .catch((error) => setError(error.message));
+    }
+  };
+  const onLoginClick = (event: React.MouseEvent) => {
+    event.preventDefault();
+    setSignupMode(!signupMode);
+  };
   return (
-    <div>AuthContainer</div>
-  )
+    <AuthWrapper>
+      <AuthForm onSubmit={onSubmit}>
+        <EmailInput
+          name="email"
+          type="text"
+          placeholder="Email"
+          required
+          value={email}
+          onChange={onChange}
+        />
+        <PasswordInput
+          name="password"
+          type="password"
+          placeholder="Password"
+          required
+          value={password}
+          onChange={onChange}
+        />
+        <ErrorText>{error}</ErrorText>
+        <SignupButton type="submit">
+          {signupMode ? "가입하기" : "로그인"}
+        </SignupButton>
+      </AuthForm>
+      <SignupWithGoogle onClick={onGoogleSignupClick}>
+        {signupMode ? "Google 계정으로 가입하기" : "Google 계정으로 로그인"}
+      </SignupWithGoogle>
+      <hr />
+      <p>
+        {signupMode
+          ? "이미 Mandalable에 가입하셨나요?"
+          : "Mandalable 계정이 없으신가요?"}
+      </p>
+      <button onClick={onLoginClick}>
+        {signupMode ? "로그인" : "가입하기"}
+      </button>
+    </AuthWrapper>
+  );
 }
