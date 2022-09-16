90d96e96dbea380617474d9852c6b8486736c50e
hwanyb <mnn9502@naver.com>
2022-09-16 10:10:00 +0900

    Feat: overviewReducer combine

--- src/modules/index.ts
+++ src/modules/index.ts
@@ -2,11 +2,13 @@ import { combineReducers } from "redux";
 import mandalartReducer from "./mandalartReducer";
 import authReducer from "./authReducer";
 import goalReducer from "./goalReducer";
+import overviewReducer from "./overviewReducer";
 
 const rootReducer = combineReducers({
   mandalartReducer,
   authReducer,
   goalReducer,
+  overviewReducer
 });
 
 export default rootReducer;
