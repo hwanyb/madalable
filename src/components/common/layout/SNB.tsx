d9dbcf3d8beaaad616cd9e2d1f6a967a5ea20293
hwanyb <mnn9502@naver.com>
2022-09-16 20:00:00 +0900

    Feat: SNB 토글 기능 추가

--- src/components/common/layout/SNB.tsx
+++ src/components/common/layout/SNB.tsx
@@ -1,15 +1,52 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link, useLocation } from "react-router-dom";
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 import { authService } from "../../../firebase";
 import { Icon } from "../../../styles/Common";
 
-const Base = styled.div`
-  width: 200px;
-  box-sizing: border-box;
-  margin-top: 30px;
+const ToggleBtn = styled(Icon)`
+  transition: all 0.5s ease-in-out;
+  color: ${(props) => props.theme.color.background};
+  position: absolute;
+  right: -20px;
+  height: 24px;
+  display: flex;
+  align-items: center;
+  border-radius: 5px;
+  &:hover {
+    background-color: ${(props) => props.theme.color.lightGray};
+    color: ${(props) => props.theme.color.fontPrimary};
+  }
 `;
 
+const Base = styled.div<{ snbIsOpened: boolean }>`
+  height: 100%;
+  box-sizing: border-box;
+  margin-top: 30px;
+  position: relative;
+  transition: all 0.5s ease-in-out;
+  &:hover ${ToggleBtn} {
+    color: ${(props) => props.theme.color.gray};
+    transition: all 0.5s ease-in-out;
+  }
+  ${(props) =>
+    props.snbIsOpened
+      ? css`
+          width: 250px;
+        `
+      : css`
+          width: 50px;
+          ${ItemText} {
+            display: none;
+          }
+          ${ItemWrapper} {
+            padding-left: 5px;
+          }
+          ${LogoutBtn} {
+            padding-left: 5px;
+          }
+        `}
+`;
 const ItemIcon = styled(Icon)`
   line-height: 40px;
   margin-right: 10px;
@@ -19,17 +56,27 @@ const ItemText = styled.h3`
   font-weight: 300;
   font-size: ${(props) => props.theme.fontSize.base};
   line-height: 40px;
+  position: relative;
+  top: 2px;
+  white-space: nowrap;
 `;
-const ItemWrapper = styled(Link)<{location: string}>`
+const ItemWrapper = styled(Link)<{ location: string }>`
+  width: 70%;
   display: flex;
   cursor: pointer;
-  color: ${(props) => props.location === props.className ? props.theme.color.white : props.theme.color.primary};
+  color: ${(props) =>
+    props.location === props.className
+      ? props.theme.color.white
+      : props.theme.color.primary};
   margin-bottom: 10px;
-  border-radius: 0 50px 50px 0;
+  border-radius: 0 10px 10px 0;
   padding-left: 30px;
   transition: all 0.2s ease;
   text-decoration: none;
-  background-color: ${(props) => props.location === props.className ? props.theme.color.primary : "transparent"};
+  background-color: ${(props) =>
+    props.location === props.className
+      ? props.theme.color.primary
+      : "transparent"};
   &:hover {
     background-color: ${(props) => props.theme.color.primary};
   }
@@ -41,11 +88,12 @@ const ItemWrapper = styled(Link)<{location: string}>`
   }
 `;
 const LogoutBtn = styled.div`
+  width: 70%;
   display: flex;
   cursor: pointer;
   color: ${(props) => props.theme.color.primary};
   margin-bottom: 10px;
-  border-radius: 0 50px 50px 0;
+  border-radius: 0 10px 10px 0;
   padding-left: 30px;
   transition: all 0.2s ease;
 
@@ -60,15 +108,33 @@ const LogoutBtn = styled.div`
   }
 `;
 export default function SNB() {
+  const [snbIsOpened, setSnbIsOpened] = useState(
+    JSON.parse(localStorage.getItem("SNB")),
+  );
+  useEffect(() => {
+    localStorage.setItem("SNB", JSON.stringify(snbIsOpened));
+  });
   const onLogoutClick = () => {
     const result = window.confirm("로그아웃하시겠습니까?");
     if (result) {
       authService.signOut();
     }
   };
+  const onToggleSnbClick = () => {
+    setSnbIsOpened(!snbIsOpened);
+  };
   const location = useLocation().pathname;
   return (
-    <Base>
+    <Base snbIsOpened={snbIsOpened}>
+      <ToggleBtn
+        onClick={onToggleSnbClick}
+        className="material-symbols-rounded"
+        id={snbIsOpened ? "close" : "open"}
+      >
+        {snbIsOpened
+          ? "keyboard_double_arrow_left"
+          : "keyboard_double_arrow_right"}
+      </ToggleBtn>
       <ItemWrapper to="/" className="/" location={location}>
         <ItemIcon className="material-symbols-rounded">home</ItemIcon>
         <ItemText>Home</ItemText>
@@ -83,7 +149,7 @@ export default function SNB() {
         </ItemIcon>
         <ItemText>Overview</ItemText>
       </ItemWrapper>
-      <ItemWrapper to="/guide" className='/guide' location={location}>
+      <ItemWrapper to="/guide" className="/guide" location={location}>
         <ItemIcon className="material-symbols-rounded">help</ItemIcon>
         <ItemText>Guide</ItemText>
       </ItemWrapper>
