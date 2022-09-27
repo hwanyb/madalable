9512f3e93dd0e174172bd2224da3d09cd61339b2
hwanyb <mnn9502@naver.com>
2022-09-27 11:30:00 +0900

    Design : Update Header style

--- src/components/common/layout/Header.tsx
+++ src/components/common/layout/Header.tsx
@@ -2,7 +2,10 @@ import React from "react";
 import styled from "styled-components";
 
 const Base = styled.header`
-  height: 50px;
+  position: fixed;
+  width: 100vw;
+  top: 0;
+  left: 0;
   padding: 30px;
   display: flex;
 `;
@@ -11,11 +14,13 @@ const LogoImg = styled.img`
   margin-right: 10px;
 `;
 const LogoText = styled.h1`
-  align-self: flex-end;
   font-family: ${(props) => props.theme.fontFamily.aggro};
   font-size: ${(props) => props.theme.fontSize.base};
   color: ${(props) => props.theme.color.primary};
-  font-weight: 500;
+  font-weight: 700;
+  display: flex;
+  flex-direction: column;
+  justify-content: end;
 `;
 
 export default function Header() {
